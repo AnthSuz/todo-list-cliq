b237ac2a04d6fda7e33d4737a87c890a84249bee
AnthSuz <56254134+AnthSuz@users.noreply.github.com>
2023-04-10 12:03:22 +0200

    create todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { CheckCircleOutline, HighlightOff } from '@material-ui/icons';
+
+interface Todo {
+  title: string,
+  completed: boolean,
+  id: number
+}
 
 function App() {
+  const [input, setInput] = useState<string>('')
+  const [todoList, setTodoList] = useState<Todo[]>([])
+
+  /**
+   * Function for add task on todo-list
+   */
+  const addTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodoList((prev) => [
+      {
+        title: input,
+        completed: false,
+        id: prev && prev[0] ? prev[0].id + 1 : 0
+      },
+      ...prev,
+    ])
+    setInput('')
+  }
+
+  /**
+   * Function for completed task on todo-list
+   */
+    const completedTask = (i: number) => {
+      const stockTodoList = [...todoList];
+      stockTodoList[i].completed = !stockTodoList[i].completed
+      setTodoList(stockTodoList)
+  }
+
+  /**
+   * Function for delete task on todo-list
+   */
+    const deleteTask = (id: number) => {
+    const stockTodoList = [...todoList];
+    stockTodoList.splice(todoList.findIndex((t) => t.id === id), 1)
+    setTodoList(stockTodoList)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Todo-List for Cliq
       </header>
+      <form onSubmit={(e) => addTask(e)}>
+        <input value={input} onChange={((e) => setInput(e.target.value))} />
+        <button disabled={!input.trim()} type="submit">Button</button>
+      </form>
+
+      <div>
+        {todoList.length ? todoList.map((todo, index) => {
+          return (
+            <span key={index} className="Task">
+              <p className={todo.completed ? 'TaskCompleted' : ''}>{todo.title}</p>
+              <CheckCircleOutline className="CheckIcon" onClick={() => completedTask(index)} />
+              <HighlightOff className="DeleteIcon" onClick={() => deleteTask(todo.id)} />
+            </span>
+          )
+        }) : <p>No task.</p>}
+      </div>
     </div>
   );
 }
